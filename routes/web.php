53785e90c1b7e1235b35ba27bca065a1049bd9dc
Henry Jr Zai <hjuniorzai@gmail.com>
2025-09-27 16:18:03 +0700

    add : feat login admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('login'); });
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('admin.login');
+Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard')->middleware('auth');
